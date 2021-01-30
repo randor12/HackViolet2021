549a18eb0de52de1a6bdb155760297889813f407
jcheng2000 <36394102+diamond8658@users.noreply.github.com>
2021-01-30 11:50:20 -0500

    Initialized account management and searchData

---
+++ account.py
@@ -0,0 +1,28 @@
+"""
+Account details
+"""
+class Account():
+    def __init__(self):
+        dataArray = {}
+        
+    # Adds a search to the search Array
+    def addSearch(search):
+        dataArray.append(search)
+    
+    # Lists all the searches an account has made
+    def listSearch():
+        for searchData in dataArray:
+            # Print out the searchData
+    
+    # Lists the last few searches an account has made
+    def getRecents(number):
+        for searchData in range(number):
+            # Print out the last "number" searches
+    
+    # Lists the last 3 searches
+    def getRecents():
+        for searchData in range(3):
+            # Print out the last 3 searches
+            
+    
+    
---
+++ searchData.py
@@ -0,0 +1,6 @@
+"""
+Holds the search data for an account
+"""
+class searchData():
+    def __init__(self):
+        
