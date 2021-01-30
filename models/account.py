274663d2cc7a829a9720c30c45b1da080620574d
jcheng2000 <36394102+diamond8658@users.noreply.github.com>
2021-01-30 13:00:02 -0500

    Update account.py

--- models/account.py
+++ models/account.py
@@ -3,31 +3,31 @@ Account details
 """
 class Account():
     def __init__(self, uname, passw, email):
-        self.dataArray = {}
+        self.posts = {}
         self.user = uname
         self.password = passw
         self.em = email
         
     # Adds a search to the search Array
-    def addSearch(self, search):
-        self.dataArray.append(search)
+    def addSearch(self, post):
+        self.posts.append(post)
     
-    # Lists all the searches an account has made
-    def listSearch(self):
-        for searchData in self.dataArray:
+    # Lists all the posts an account has made
+    def listPosts(self):
+        for searchData in self.posts:
             # Print out the searchData
             pass
     
-    # Lists the last few searches an account has made
+    # Lists the last few posts an account has made
     def getRecents(self, number):
         for searchData in range(number):
-            # Print out the last "number" searches
+            # Print out the last "number" posts
             pass
     
-    # Lists the last 3 searches
+    # Lists the last 3 posts
     def getRecents(self):
         for searchData in range(3):
-            # Print out the last 3 searches
+            # Print out the last 3 posts
             pass
             
     
