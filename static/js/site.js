0b6a3b8a7ad08d7c7c78d19bdf49dae1872bb88d
mattgonley <mattgonley@gmail.com>
2021-01-31 09:43:13 -0500

    chat update

--- static/js/site.js
+++ static/js/site.js
@@ -3,3 +3,28 @@
  */
 
 console.log("Hello, World!");
+
+function send() {
+        var txt = document.getElementById("entryField").value
+        document.getElementById("entryField").value = ""
+        var node =  document.createElement("li");
+        var right = document.getElementsByClassName("chat-right")
+        var left = document.getElementsByClassName("chat-left")
+        var l = left.length
+        var r = right.length
+        var rightItem = right[r -1].cloneNode(true)
+        var leftItem = left[l -1].cloneNode(true)
+        rightItem.getElementsByClassName("chat-text")[0].innerText = txt
+        var tt = 'Im grand, thanks for asking. How has business  been? And sure, I would be down to get drinks later if you are.'
+        leftItem.getElementsByClassName("chat-text")[0].innerText = tt
+        document.getElementById("chatting").appendChild(rightItem)
+
+        setTimeout(slll, 5000 ,leftItem)
+}
+
+function slll(elem) {
+    const sleep = promisify(setTimeout)
+    sleep(5000).then(() => {
+        document.getElementById("chatting").appendChild(elem)
+
+}
